86c883dbda36ecdd70958681d7dd73f57fa8404b
lakrestofer <lakrestofer@gmail.com>
2023-11-19 12:32:48 +0100

    remove old migrations

--- src-tauri/migration/src/lib.rs
+++ src-tauri/migration/src/lib.rs
@@ -1,6 +1,5 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
 mod m20231111_123534_sources;
 
 pub struct Migrator;
@@ -9,7 +8,6 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20231111_123534_sources::Migration),
         ]
     }
